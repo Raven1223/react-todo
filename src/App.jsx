91bea7f641385ac7121da09a83a7f846c6cf43ce
Raven1223 <Zulu.shaun4@gmail.com>
2024-11-13 02:07:08 -0600

    Lesson 1.2 React Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import * as React from 'react';
-const todoList = [
-  {id: 1, title: "Complete my next CTD assignments on time" }, {id : 2, title: "Schedule and Attend mentor sessions" }, 
-  {id: 3, title: "Finish setting up the cloud for my capstone" }
-];
+import TodoList from './TodoList';  // Import TodoList component
+import AddTodoForm from './AddTodoForm';
+
 function App(){
   return (
     <div>
       <h1>Todo List</h1>
-      <ul>{todoList.map(function(todoList){
-        return <li key = {todoList.title}>{todoList.title}</li>;
-      })}
-      </ul>
+      <AddTodoForm/>
     </div>
   );
 }
---
+++ src/TodoList.jsx
@@ -0,0 +1,17 @@
+//import * as React from 'react';
+
+const todoList = [
+    {id: 1, title: " my next CTD assignments on time" }, {id : 2, title: "Schedule and Attend mentor sessions" }, 
+    {id: 3, title: "Finish setting up the cloud for my capstone" }
+  ];
+
+function TodoList(){
+    return (
+        <ul>{todoList.map(function(todoList){
+            return <li key = {todoList.title}>{todoList.title}</li>;
+          })}
+          </ul> 
+    );
+}
+
+export default TodoList;
