08b97fa3f0a3ed91573642c324b1366e09160016
Raven1223 <Zulu.shaun4@gmail.com>
2024-11-13 02:09:04 -0600

    lesson 1.2 React Components

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,7 +1,7 @@
 //import * as React from 'react';
 
 const todoList = [
-    {id: 1, title: " my next CTD assignments on time" }, {id : 2, title: "Schedule and Attend mentor sessions" }, 
+    {id: 1, title: "Complete my next CTD assignments on time" }, {id : 2, title: "Schedule and Attend mentor sessions" }, 
     {id: 3, title: "Finish setting up the cloud for my capstone" }
   ];
 
